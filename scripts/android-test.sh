95593a880c149427e9a932c1b5c419a84ab39e43
Dethbug <mccjul@gmail.com>
2016-07-20 18:52:54 -0400

    scripts added

---
+++ scripts/android-test.sh
@@ -0,0 +1,22 @@
+# taken from https://github.com/facebook/react-native/scripts/circle-ci-android-setup.sh
+
+function getAndroidSDK {
+  export PATH="$ANDROID_HOME/platform-tools:$ANDROID_HOME/tools:$PATH"
+
+  DEPS="$ANDROID_HOME/installed-dependencies"
+
+  if [ ! -e $DEPS ]; then
+    echo no | android create avd -n testAVD -f -t android-19 --abi default/armeabi-v7a &&
+    touch $DEPS
+  fi
+}
+
+function waitForAVD {
+  local bootanim=""
+  export PATH=$(dirname $(dirname $(which android)))/platform-tools:$PATH
+  until [[ "$bootanim" =~ "stopped" ]]; do
+    sleep 5
+    bootanim=$(adb -e shell getprop init.svc.bootanim 2>&1)
+    echo "emulator status=$bootanim"
+  done
+}
