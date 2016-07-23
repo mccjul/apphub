8ac2be4fbc1693e42a087d7bd5e4039f4273f7fd
Dethbug <mccjul@gmail.com>
2016-07-23 14:47:40 -0400

    Revert "removing integration testing test"
    This reverts commit 80a7ccbbf297f7063f75a12cebaecc73a7774dc2.

---
+++ android/IntegrationTests/android/app/src/main/java/com/integrationtests/MainActivity.java
@@ -0,0 +1,94 @@
+package com.integrationtests;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import io.apphub.*;
+
+public class MainActivity extends Activity implements DefaultHardwareBackBtnHandler {
+
+    private ReactInstanceManager mReactInstanceManager;
+    private ReactRootView mReactRootView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AppHub.initialize(this);
+        AppHub.setLogLevel(AppHubLogLevel.DEBUG);
+
+        AppHubApplication application = new AppHubApplication("gled42aSkRpNkHbZYpqq");
+
+        String bundleAssetPath = application.getBuildManager().getLatestBuild().
+                getBundleAssetPathWithName("index.android.bundle");
+        application.getBuildManager().addNewBuildListener(new AppHubNewBuildListener() {
+            @Override
+            public void onNewBuild(AppHubBuild build) {
+                System.out.println("NEW BUILD!!!");
+            }
+        });
+        mReactRootView = new ReactRootView(this);
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setJSBundleFile(bundleAssetPath)
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        mReactRootView.startReactApplication(mReactInstanceManager, "IntegrationTests", null);
+
+        setContentView(mReactRootView);
+    }
+
+    @Override
+    public boolean onKeyUp(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_MENU && mReactInstanceManager != null) {
+            mReactInstanceManager.showDevOptionsDialog();
+            return true;
+        }
+        return super.onKeyUp(keyCode, event);
+    }
+
+    @Override
+    public void onBackPressed() {
+      if (mReactInstanceManager != null) {
+        mReactInstanceManager.onBackPressed();
+      } else {
+        super.onBackPressed();
+      }
+    }
+
+    @Override
+    public void invokeDefaultOnBackPressed() {
+      super.onBackPressed();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (mReactInstanceManager != null) {
+            mReactInstanceManager.onResume(this, this);
+        }
+    }
+}
